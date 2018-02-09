02813aa3e408c70e9531a87e1e072cac4c69774b
ngesa marvin <marvinngesa@gmail.com>
2018-02-09 09:41:11 +0300

    update mainactivity

--- app/src/main/java/io/droidmarvin/pushconfignotification/MainActivity.java
+++ app/src/main/java/io/droidmarvin/pushconfignotification/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -64,4 +66,34 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        updateConfg();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.updateConfig) {
+            updateConfg();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
