02668b20bb6cbd887eb512dffd2d5c15bcc708fb
ngesa marvin <marvinngesa@gmail.com>
2018-02-09 09:28:36 +0300

    get a Remote Config object that will be used to cache your local values and get remote values as well.

--- app/src/main/java/io/droidmarvin/pushconfignotification/MainActivity.java
+++ app/src/main/java/io/droidmarvin/pushconfignotification/MainActivity.java
@@ -3,11 +3,24 @@ package io.droidmarvin.pushconfignotification;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseRemoteConfig firebaseRemoteConfig;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Loading the Config Parameters or instance variables
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        firebaseRemoteConfig.setDefaults(R.xml.remote_config_params);
+
+        firebaseRemoteConfig.setConfigSettings(
+                new FirebaseRemoteConfigSettings.Builder()
+                        .setDeveloperModeEnabled(true)
+                        .build() );
     }
 }
