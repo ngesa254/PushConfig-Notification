eef458e6732435be35f155fc85df9f7454efc354
ngesa marvin <marvinngesa@gmail.com>
2018-02-09 08:46:38 +0300

    update MyFirebaseMessagingService.java to extend FirebaseMessagingService

--- app/src/main/java/io/droidmarvin/pushconfignotification/MyFirebaseMessagingService.java
+++ app/src/main/java/io/droidmarvin/pushconfignotification/MyFirebaseMessagingService.java
@@ -1,4 +1,38 @@
 package io.droidmarvin.pushconfignotification;
 
-public class MyFirebaseMessagingService {
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Log.d("Message", "Message received ["+ remoteMessage +"]");
+
+        //Create Notification
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1,
+                intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new
+                NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle("Message")
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notificationBuilder.build());
+    }
 }
