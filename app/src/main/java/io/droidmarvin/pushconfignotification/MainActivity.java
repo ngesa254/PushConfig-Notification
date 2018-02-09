07512216cfc565096e4a4082a17b03ea93ea5777
ngesa marvin <marvinngesa@gmail.com>
2018-02-09 09:29:58 +0300

    create a method to fetch and apply the configuration from the Firebase Console.

--- app/src/main/java/io/droidmarvin/pushconfignotification/MainActivity.java
+++ app/src/main/java/io/droidmarvin/pushconfignotification/MainActivity.java
@@ -1,7 +1,10 @@
 package io.droidmarvin.pushconfignotification;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
@@ -22,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
                 new FirebaseRemoteConfigSettings.Builder()
                         .setDeveloperModeEnabled(true)
                         .build() );
+    }
+
+    private void applyConfig() {
+        //Get the widget from XML layout
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.layout);
+        TextView textview = (TextView) findViewById(R.id.textView);
+
+        //Get the values form Firebase remote configuration
+        String welcomeText = firebaseRemoteConfig.getString("welcome_text");
+        String welcomeTextColor = firebaseRemoteConfig.getString("welcome_text_color");
+        String layoutColor = firebaseRemoteConfig.getString("bg_color");
+
+        // Set the properties from firebase remote configuration
+        // If any value not set in firebase remote configuration then it gets from default set
+        layout.setBackgroundColor(Color.parseColor(layoutColor));
+        textview.setText(welcomeText);
+        textview.setTextColor(Color.parseColor(welcomeTextColor));
+
     }
 }
